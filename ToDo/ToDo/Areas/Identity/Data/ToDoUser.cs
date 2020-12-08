cf885c0b481321d9d074e5482d2814615997d876
mimz-skywalker <mimzitoo98@gmail.com>
2020-12-08 04:56:07 +0200

    adding login and register

---
+++ ToDo/ToDo/Areas/Identity/Data/ToDoUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ToDo.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ToDoUser class
+    public class ToDoUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public String FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public String LastName { get; set; }
+
+    }
+}
---
+++ ToDo/ToDo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDo.Areas.Identity.Data;
+using ToDo.Data;
+
+[assembly: HostingStartup(typeof(ToDo.Areas.Identity.IdentityHostingStartup))]
+namespace ToDo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ToDoUser>(options => 
+                {
+
+                    options.SignIn.RequireConfirmedAccount = false;
+
+                    //Password Requirements
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequiredUniqueChars = 1;
+                    options.Password.RequiredLength = 6;
+
+                })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
